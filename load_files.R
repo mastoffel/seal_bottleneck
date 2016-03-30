1e89e5fd2f8f4dc141840aee5b9ef5877cf90be0
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-03-30 08:59:33 +0200

    structure output wrangling

--- load_files.R
+++ load_files.R
@@ -1,8 +1,7 @@
-
-# load and clean --------------
 library(readxl)
-library(stringr)
-dataset_names <- c(1:22, 51:56) #see overview_data.xslx file in main folder
+
+# sheet numbers to load
+dataset_names <- c(1:29) 
 
 load_dataset <- function(dataset_number) {
     read_excel("../data/all_data.xlsx", sheet = paste("dataset", dataset_number), na = "x")
@@ -10,93 +9,33 @@ load_dataset <- function(dataset_number) {
 
 # load all datasets
 all_data <- lapply(dataset_names, load_dataset)
-all_data[[12]] <-  read_excel("../data/all_data.xlsx", sheet = "dataset 12-2", na = "x")
+all_data[[12]] <-  read_excel("../data/all_data_HW.xlsx", sheet = "dataset 12-2", na = "x")
 
 # extract all_species names
 all_species_names <- unlist(lapply(all_data, function(x) out <- names(x)[1]))
 all_species_names[12] <- "Crabeater seal recoded"
 all_species_names[23] <- "South American (Falklands) sea lion"
 
-# change names of datasets to avoid spaces, brackets and other weird symbols
-all_species_names <- str_replace_all(all_species_names, "dataset", "")
-all_species_names <- str_replace_all(all_species_names, "data", "")
-all_species_names <- str_replace_all(all_species_names, "  ", "_")
-all_species_names <- str_trim(all_species_names)
-all_species_names <- str_replace_all(all_species_names, " ", "_")
-all_species_names <- str_replace_all(all_species_names, "'", "")
-all_species_names<- str_replace_all(all_species_names, "\\(", "")
-all_species_names <- str_replace_all(all_species_names, "\\)", "")
-
 # name all datasheets in the list with the appropriate species names
 names(all_data) <- all_species_names
 
-# clean sheets --------------------------------------------------------------------------
-# function to create a vector of loci names with _a and _b for the alleles
-format_names <- function(data_sheet)  {
-    # delete_NA cols
-    data_sheet <- Filter(function(x)!all(is.na(x)), data_sheet)
-    # clean names
-    loc_names <- as.character(data_sheet[1, 3:ncol(data_sheet)]) %>%
-        str_replace_all(" ", "_") %>%
-        str_replace_all( "\\." , "") 
-         for (i in seq(from = 2, to = length(loc_names), by = 2)) {
-                 loc_names[i] <- paste(loc_names[i-1], "_b", sep = "")
-                 loc_names[i-1] <- paste(loc_names[i-1], "_a", sep = "")
-         }
-    # loc_names <- loc_names[seq(from = 1, to = length(loc_names), by = 2)]
-    loc_names
+extract_genotypes <- function(data_sheet) {
+    data_sheet_2 <- data_sheet[3:nrow(data_sheet), 3:ncol(data_sheet)]
+    names(data_sheet_2) <- 1:ncol(data_sheet_2)
+    data_sheet_2 <- data_sheet_2[rowSums(is.na(data_sheet_2)) != ncol(data_sheet_2), ]
+    # row.names(data_sheet_2) <- data_sheet_2[[1]]
+    out <- data_sheet_2
 }
 
-all_names <- lapply(all_data, format_names)
 
-# extract the correct rows and assign the locus names
-format_geno <- function(data_sheet) {
-    loc_names <- format_names(data_sheet)
-    if (any(loc_names == "NA"))  {
-        loc_names <- loc_names[-which((loc_names == "NA_a") | (loc_names == "NA_b") | (loc_names == "NA"))]
-    }
-    # delete headers
-    data_sheet <- data_sheet[3:nrow(data_sheet), ]
-    # delete_NA cols
-    data_sheet <- Filter(function(x)!all(is.na(x)), data_sheet)
-    # names
-    names(data_sheet) <- c("id", "pop", loc_names)
-    # delete NA rows
-    data_sheet <- data_sheet[rowSums(is.na(data_sheet))!=ncol(data_sheet), ]
-    # make pop numeric
-    # data_sheet$pop <- as.numeric(as.factor(data_sheet$pop))
-    data_sheet
-}
+# just pure genotypes
+all_genotypes <- lapply(all_data, extract_genotypes)
+names(all_genotypes) <- all_species_names
 
-library(dplyr)
+library(inbreedR)
+all_sMLH_var <- as.numeric(lapply(all_genotypes, function(x) out <- var(sMLH(convert_raw(x)), na.rm = TRUE)))
+all_g2_vals <- as.numeric(lapply(all_genotypes, function(x) out <- g2_microsats(convert_raw(x))$g2))
 
-# format everything
-seal_data <- lapply(all_data, format_geno)
-
-# tbl_df is a better data.frame
-seal_data <- lapply(seal_data, tbl_df)
-
-# change pop to factor
-seal_data <- lapply(seal_data, function(x) {
-                                x$pop <- as.factor(x$pop)
-                                return(x)})
-
-# how many locations? --not necessarily right, to check
-number_of_populations <- (lapply(seal_data, function(x) length(levels(x$pop))))
-names(number_of_populations) <- names(seal_data)
-
-
-# correction
-seal_data[[3]][469, 3] <- NA
-seal_data[[3]][469, 4] <- NA
-
-# save
-save(seal_data, file = "seal_data.RData")
-
-
-
-
-
-
-
-
+plot(all_g2_vals)
+text(1:28, all_g2_vals, all_species_names, cex = 0.7, pos = 4)
+?convert_raw
--- structure-output-analysis.R
+++ structure-output-analysis.R
@@ -14,8 +14,10 @@ seal_species_names <- names(all_seals)
 
 # create folder for STRUCTURE summary plots
 system("mkdir cluster_summary_plots")
+system("mkdir structure_result_tables")
 
 seals_structure_summary <- list()
+runs_to_df <- list()
 for (i in 1:length(seal_species_names)) {
     # lists all files in structure output folder 
     all_files <- list.files(paste0(path_to_structure_out, seal_species_names[i]))
@@ -27,19 +29,127 @@ for (i in 1:length(seal_species_names)) {
     # pophelper summary functions
     structure_summary <- tabulateRunsStructure(files=struc_out_paths) %>%
         summariseRunsStructure() %>%
-        evannoMethodStructure(exportplot = T, imgtype = "pdf")
+        evannoMethodStructure(exportplot = T, imgtype = "pdf", writetable = TRUE)
     # move plot into plot folder and rename it
     system(paste0("mv evannoMethodStructure.pdf cluster_summary_plots/", seal_species_names[i], ".pdf"))
-    
+    system(paste0("mv evannoMethodStructure.txt structure_result_tables/", seal_species_names[i], ".txt"))
+
+    runs_to_df[[i]] <- runsToDfStructure(struc_out_paths)
     seals_structure_summary[[i]] <- structure_summary
-    names(seals_structure_summary[i]) <- seal_species_names[i]
+    names(seals_structure_summary)[i] <- seal_species_names[i]
 }
+names(seals_structure_summary)
 
 
+
+system("mkdir clumpp")
+for (i in 1:length(seal_species_names)) {
+    # lists all files in structure output folder 
+    all_files <- list.files(paste0(path_to_structure_out, seal_species_names[i]))
+    # lists relevant files
+    struc_out_names <- all_files[str_detect(all_files, "results_job_T.+f")]
+    # creates character vector containing path to relevant files
+    struc_out_paths <- str_c(paste0(path_to_structure_out, seal_species_names[i], "/"), struc_out_names)
+    # basic usage
+    clumppExportStructure(files=struc_out_paths, prefix = seal_species_names[i])
+    # create species directory in clumpp
+    system(paste0("mkdir clumpp/", seal_species_names[i]))
+    # copy all pophelper-prepared structure folders into clumpp/species
+    system(paste0("mv ", seal_species_names[i], "_K* clumpp/", seal_species_names[i]))
+    # copy CLUMPP executable into every folder
+    system(paste0("for i in clumpp/", seal_species_names[i], "/", seal_species_names[i], "_K*; do cp /Users/martin/programs/CLUMPP $i; done"))
+    # for computation time reasons remove K > 6
+    system(paste0("rm -r clumpp/", seal_species_names[i], "/*_K10"))
+    system(paste0("rm -r clumpp/", seal_species_names[i], "/*_K[7-9]"))
+    # execute CLUMPP (the brackets activate a secondary shell)
+    # system(paste0('for i in clumpp/', seal_species_names[i], '/*/ ; do (cd "$i" && /Users/martin/program/CLUMPP); done'))
+    system(paste0('for i in clumpp/', seal_species_names[i], '/*/ ; do (cd "$i" && ./CLUMPP); done'))
+} 
+
+
+# collectCLumppOutput and plot
+system("mkdir structure_assignment_plots")
+for (i in 1:length(seal_species_names)) {
+    current_dir <- getwd()
+    setwd(paste0("/Users/martin/Dropbox/projects/current/bottleneck/seal_bottleneck_analysis/clumpp/",
+                 seal_species_names[i]))
+    # function to summarize CLUMPP output
+    collectClumppOutput(prefix = seal_species_names[i], filetype = "aligned")
+    # make list of all files
+    slist <- list.files(paste0(seal_species_names[i], "-aligned/"))
+    # create paths to all files
+    path_to_slist <- str_c(paste0(seal_species_names[i], "-aligned/"), slist)
+    # plot pdfs
+    plotRuns(files=path_to_slist, imgoutput = "tab", imgtype = "pdf", sortind = "all")
+    # create folder for pdfs
+    system(paste0("mkdir ../../structure_assignment_plots/", seal_species_names[i]))
+    # move plots to folders
+    system(paste0("mv *pdf ../../structure_assignment_plots/", seal_species_names[i]))
+    # go back to working directory
+    setwd(current_dir)
+}
+    
+    
 # rule: if mean estimated ln probability of data is highest at K=1, decide for K=1
 #       if not, use evanno-method (delta K) to decide which k
 
-rtd <- runsToDfStructure(files = struc_out_paths)
+optimal_k <- function(x) {
+    elpd <- which.max(x$elpdmean)
+    if (elpd == 1) {
+        return(1)
+    } else {
+    delta_k <- which.max(x$deltaK)
+    delta_k
+    }
+}
 
-clumppExportStructure(files = struc_out_paths)
+clustering <- lapply(seals_structure_summary, optimal_k)
+names(clustering) <- names(all_seals)
+best_clustering <- unlist(clustering)
 
+# output best k's
+both_k <- function(x) {
+    elpd <- which.max(x$elpdmean)
+    deltaK <- which.max(x$deltaK)
+    ks <- c(lnk = x$k[elpd], deltak = x$k[deltaK])
+}
+
+ks <- do.call(rbind, lapply(seals_structure_summary, both_k))
+# write.table(ks, "best_k.txt", sep = "\t")
+
+
+# extract_largest_cluster and name clusters by K (e.g. K_10)
+names(runs_to_df) <- seal_species_names
+first_run_each_k <- seq(from = 1, to = 50, by = 5)
+
+extract_and_name_runs <- function(x) {
+    runs <- x[first_run_each_k]
+    ks <- unlist(lapply(runs, ncol))
+    names(runs) <- paste0("K_", ks)
+    runs
+}
+runs_to_df_reduced <- lapply(runs_to_df, extract_and_name_runs)
+
+# create data.frame with optimal K´s 
+clusters <- as.data.frame(best_clustering) 
+names(runs_to_df_reduced)
+
+get_cluster_info <- function(species, runs_to_df_reduced, clusters) {
+    species_runs <- runs_to_df_reduced[[species]]
+    num_clust <- clusters[species, ]
+    if (num_clust > 1) {
+        best_clust <- species_runs[[paste0("K_", num_clust)]]
+        cluster <- apply(best_clust, 1, which.max)
+        assign_to_cluster <- data.frame(cluster)
+    } else {
+        cluster <- rep(1, nrow(species_runs[[1]]))
+        assign_to_cluster <- data.frame(cluster)
+    }
+    assign_to_cluster
+}
+
+lapply(seal_species_names[1:6], get_cluster_info, runs_to_df_reduced, clusters)
+# check seal_species_names[7] --> weird species runs
+
+which(cluster > 1)
+names(runs_to_df_simple)
